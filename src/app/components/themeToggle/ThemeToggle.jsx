5cecd35bce35f755865c921276e7072ef1a629eb
Ryan Palmer <ryandanpalmer@gmail.com>
2024-10-24 13:03:15 -0600

    fixing errors in themtoggle

--- src/app/components/themeToggle/ThemeToggle.jsx
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,11 +1,13 @@
 import style from './themeToggle.module.css';
+import Image from 'next/image';
 
 const ThemeToggle = () => {
-    return (
-        <div className={style.container}>ThemeToggle</div>
-        <Image src="/moon.png" alt = "moon" width={14} height={14}/>
-        <Image src="/sun.png" alt = "sun" width={14} height={14}/>
-    )
+    return 
+        <div className={style.container}>
+            <Image src="/moon.png" alt = "moon" width={14} height={14}/>
+            <Image src="/sun.png" alt = "sun" width={14} height={14}/>
+        </div>;
+    
 };
 
 export default ThemeToggle;
