0f5d58234f98f5371b6b3847f02a2534abf6baee
Ryan Palmer <ryandanpalmer@gmail.com>
2024-10-24 13:31:54 -0600

    working themetoggle adding themecontext

--- src/app/components/themeToggle/ThemeToggle.jsx
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,12 +1,15 @@
-import style from './themeToggle.module.css';
+import styles from './themeToggle.module.css';
 import Image from 'next/image';
 
 const ThemeToggle = () => {
-    return 
-        <div className={style.container}>
-            <Image src="/moon.png" alt = "moon" width={14} height={14}/>
-            <Image src="/sun.png" alt = "sun" width={14} height={14}/>
-        </div>;
+    return (
+        <div className={styles.container}>
+        <Image src="/moon.png" alt = "moon" width={14} height={14}/>
+        <div className={styles.ball}></div>
+        <Image src="/sun.png" alt = "sun" width={14} height={14}/>
+    </div>
+    );
+      
     
 };
 
