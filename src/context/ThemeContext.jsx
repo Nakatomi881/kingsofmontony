cb391d2d8bb1cb237e18db643df995c84757b913
Ryan Palmer <ryandanpalmer@gmail.com>
2024-10-27 15:21:12 -0600

    fixed themetoggle

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,8 +1,36 @@
-import { createContext } from "react";
+"use client";
+
+import { createContext, useState, useEffect } from "react";
 
 
 export const ThemeContext = createContext();
 
+const getFromLocalStorage = () => {
+
+    if (typeof window !== "undefined") {
+        const value = localStorage.getItem("theme");
+        return value || "light";
+    }
+    return "light";
+    
+};
+
 export const ThemeContextProvider = ({ children }) => {
-    return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
-}
+    const [theme, setTheme] = useState(() => getFromLocalStorage());
+
+    const toggle = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    };
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            localStorage.setItem("theme", theme);
+        }
+    }, [theme]); // Correct syntax: comma before the dependency array
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggle }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
