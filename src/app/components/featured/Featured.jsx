13c509cdc627bcf8e4518122a4bb862e1ef027cf
Ryan Palmer <ryandanpalmer@gmail.com>
2024-10-31 14:58:10 -0600

    updating feature section

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -3,7 +3,23 @@ import styles from "./featured.module.css"
 
 const Featured = () => {
     return (
-        <div className={styles.container}>Featured</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Discover my story with testing and coding.</h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src="/thrown.png" alt="thrown" fill className={styles.Image}/>
+                    
+                </div>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.postTitle}>This will be interesting my journey as a developer</h1>
+                    <p className={styles.postDesc}>
+                        I will be using this site for R&D and my learning on how to be a more effective testing engineer
+                    </p>
+                    <button className={styles.button}>Read More</button>
+                </div>
+            </div>
+            
+        </div>
     )
 }
 
--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,28 +1,32 @@
 import React from "react";
-import styles from "./navbar.module.css"
+import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import ThemeToggle from "../themeToggle/ThemeToggle"
+import ThemeToggle from "../themeToggle/ThemeToggle";
 import AuthLinks from "../authLinks/AuthLinks";
 
 const Navbar = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.social}>
-                <Image src="/linkedin.png" alt="LinkedIn" width={24} height={24}/>
-            </div>
-            <div className={styles.logo}>Kings of Monotony</div>
-            <div className={styles.links}>
-                <ThemeToggle />
-                <Link href="/" className={styles.link}>Blog</Link>
-                <Link href="/" className={styles.link}>Contact</Link>
-                <Link href="/" className={styles.link}>About</Link>
-                <AuthLinks />
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/linkedin.png" alt="LinkedIn" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>Kings of Monotony</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/" className={styles.link}>
+          Blog
+        </Link>
+        <Link href="/" className={styles.link}>
+          Contact
+        </Link>
+        <Link href="/" className={styles.link}>
+          About
+        </Link>
+        <AuthLinks />
+      </div>
+    </div>
+  );
+};
 
-
-            </div>
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
