e18934784e5bb92b2544908a163e3230d6374c4e
Ryan Palmer <ryandanpalmer@gmail.com>
2024-11-29 14:21:29 -0700

    adding comment section

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,53 +1,68 @@
-import React from 'react'
-import styles from './singlePage.module.css'
-import Menu from '../components/menu/Menu'
-import Image from 'next/image'
-import Likedislike from '../components/comments/Comments'
+import React from "react";
+import styles from "./singlePage.module.css";
+import Menu from "../components/menu/Menu";
+import Image from "next/image";
+import Comments from "../components/comments/Comments";
 
 const SinglePage = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.infoContainer}>
+      <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-            <h1 className={styles.title}>Coding journey</h1>
-            <div className={styles.user}>
-                <div className={styles.userImageContainer}>
-                    <Image src="/codingPost.webp" alt="coding" fill className={styles.avatar}/>
-                </div>
-                <div className={styles.userTextContainer}>
-                <span className={styles.username}>Ryan Palmer</span>
-                <span className={styles.date}>11.16.2024</span>
-                </div>
+          <h1 className={styles.title}>Coding journey</h1>
+          <div className={styles.user}>
+            <div className={styles.userImageContainer}>
+              <Image
+                src="/codingPost.webp"
+                alt="coding"
+                fill
+                className={styles.avatar}
+              />
             </div>
+            <div className={styles.userInfo}>
+              <span className={styles.username}>Ryan Palmer</span>
+              <span className={styles.date}>11.16.2024</span>
+            </div>
+          </div>
         </div>
         <div className={styles.imageContainer}>
-        <Image src="/codingPost2.webp" alt="coding" fill className={styles.image}/>
+          <Image
+            src="/codingPost2.webp"
+            alt="coding"
+            fill
+            className={styles.image}
+          />
         </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.post}>
+          <div className={styles.description}>
+            <p>
+              Learning how to code is an empowering journey that opens up
+              endless opportunities in today’s digital world. It starts with
+              understanding the basics—how computers interpret commands, the
+              logic behind problem-solving, and using programming languages like
+              Python, JavaScript, or others. Initially, coding might feel like
+              learning a new language, with syntax and structure that seem
+              unfamiliar. However, with consistent practice and determination,
+              the pieces start coming together. Projects, challenges, and
+              debugging sessions become stepping stones to growth. Learning to
+              code fosters creativity, critical thinking, and resilience, and
+              ultimately leads you to build everything from small scripts to
+              powerful applications that solve real-world problems. It’s a
+              process of constant learning, but it’s also deeply rewarding as
+              each step brings you closer to being able to create and shape
+              technology
+            </p>
+          </div>
+          <div className={styles.comment}>
+            <Comments />
+          </div>
         </div>
-        <div className={styles.content}>
-            <div className={styles.post}>
-                <div className={styles.description}>
-                <p>
-                    I have been learning coding for the past six years.
-                    Long journey and a challenging for sure 
-                </p>
-                <p>
-                    I have been learning coding for the past six years.
-                    Long journey and a challenging for sure 
-                </p>
-                <p>
-                    I have been learning coding for the past six years.
-                    Long journey and a challenging for sure 
-                </p>
-                </div>
-                <div className={styles.comment}>
-                    <Comments/>
-                </div>
-            </div>
-            <Menu />
-        </div>
+        <Menu />
+      </div>
     </div>
   );
 };
 
-export default SinglePage
+export default SinglePage;
--- src/app/components/comments/Comments.jsx
+++ src/app/components/comments/Comments.jsx
@@ -1,24 +1,48 @@
-import styles from './comments.module.css'
-import Link from 'next/link'
+import styles from "./comments.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Comments = () => {
-  const status = "authenticated"
+  const status = "authenticated";
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
       {status === "authenticated" ? (
         <div className={styles.write}>
-          <textarea placeholder='write a comment...' className={styles.input}/>
+          <textarea
+            placeholder="write a comment..."
+            className={styles.input}
+          ></textarea>
 
-         <button className={styles.button}>Send</button>
+          <button className={styles.button}>Send</button>
         </div>
       ) : (
         <Link href="/login">Login to write a comment</Link>
       )}
-        
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/thrown.webp"
+              alt=""
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>Ryan Palmer</span>
+              <span className={styles.date}>11.28.2024</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Lorem, impsu dolor sit amet consectetur adisicing Lorem, impsu dolor
+            sit amet consectetur adisicing Lorem, impsu dolor sit amet
+            consectetur adisicing
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
 
-
-export default Comments
+export default Comments;
